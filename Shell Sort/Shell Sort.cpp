c233ba8d8afe2a458ae5b3da7bd8b26df48c6f73
Abdullah Maqsood <abdullahsbusiness123@gmail.com>
2023-08-04 20:51:07 +0500

    Updated Shell Sorting algorithm with a few changes

--- Shell Sort/Shell Sort.cpp
+++ Shell Sort/Shell Sort.cpp
@@ -7,31 +7,16 @@ int main() {
 
 	int arr[8] = { 8,7,6,5,4,3,2,1 };
 	int halfSize = 8;
-	//while(halfSize/2 > 0){
-	//	halfSize /= 2;
-	//	for (int i = 0; i < 8; i++)
-	//	{
-	//		int check = i - halfSize;		//this checks if the range is within the range of the array i.e 0-7;
-	//		if ((check >= 0 && check < 8) && arr[i] < arr[i - halfSize]) {
-	//			int temp = arr[i];
-	//			arr[i] = arr[i - halfSize];
-	//			arr[i - halfSize] = temp;
-	//		}
-	//	}
-	//}
 	
 	while(halfSize/2 > 0){
 		halfSize /= 2;
-		for (int j = halfSize; j < 8; j++)
+		for (int i = halfSize; i < 8; i++)
 		{
-			int temp = arr[j];
-			int i = 0;
-
-			for (int i = j; i > halfSize && arr[i- halfSize] > temp ; i-=halfSize)
-			{
+			if (arr[i] < arr[i - halfSize]) {
+				int temp = arr[i];
 				arr[i] = arr[i - halfSize];
+				arr[i - halfSize] = temp;
 			}
-			arr[i] = temp;
 		}
 	}
 
